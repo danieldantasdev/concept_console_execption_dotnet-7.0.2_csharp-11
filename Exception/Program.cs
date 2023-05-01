1d4d9354877f4971f1a29e716e226ebc1146671a
lobinhodev <contatolobinhodev@gmail.com>
2023-05-01 00:10:16 -0300

    init: commit initial

---
+++ Exception/Program.cs
@@ -0,0 +1,26 @@
+﻿string number = "seven";
+string valueNull = null;
+var valueLong = long.MaxValue.ToString();
+
+try
+{
+    var formatException = int.Parse(number);
+    var argumentNullException = int.Parse(valueNull);
+    var overflowException = int.Parse(valueLong);
+}
+catch (FormatException formatException)
+{
+    Console.WriteLine($"formatException: {formatException.Message}");
+}
+catch (ArgumentNullException argumentNullException)
+{
+    Console.WriteLine($"argumentNullException: {argumentNullException.Message}");
+}
+catch (OverflowException overflowException)
+{
+    Console.WriteLine($"overflowException: {overflowException.Message}");
+}
+finally
+{
+    Console.WriteLine("Ever.");
+}
